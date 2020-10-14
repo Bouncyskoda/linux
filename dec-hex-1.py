59fb6b5ffe2db544e7cd352e34a389d9a7b70582
Adam <diamondminecraftadam638@gmail.com>
2020-10-14 10:24:28 -0700

    Create dec-hex-1.py

---
+++ dec-hex-1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/enc python3
+# Base 10 to Base 16 Adam Skoda dex-hex-1.py
+print("DECIMAL", 0,"= HEXADECIMAL", hex(0))
+print("DECIMAL", 1,"= HEXADECIMAL", hex(1))
+print("DECIMAL", 2,"= HEXADECIMAL", hex(2))
+print("DECIMAL", 3,"= HEXADECIMAL", hex(3))
+print("DECIMAL", 4,"= HEXADECIMAL", hex(4))
+print("DECIMAL", 5,"= HEXADECIMAL", hex(5))
+print("DECIMAL", 6,"= HEXADECIMAL", hex(6))
+print("DECIMAL", 7,"= HEXADECIMAL", hex(7))
+print("DECIMAL", 8,"= HEXADECIMAL", hex(8))
+print("DECIMAL", 9,"= HEXADECIMAL", hex(9))
+print("DECIMAL", 10,"= HEXADECIMAL", hex(10))
+print("DECIMAL", 11,"= HEXADECIMAL", hex(11))
+print("DECIMAL", 12,"= HEXADECIMAL", hex(12))
+print("DECIMAL", 13,"= HEXADECIMAL", hex(13))
+print("DECIMAL", 14,"= HEXADECIMAL", hex(14))
+print("DECIMAL", 15,"= HEXADECIMAL", hex(15))
+print("DECIMAL", 16,"= HEXADECIMAL", hex(16))
+'''
+10^3    10^2   10^1    10^0
+1,000    100    10       1
+
+16^7        16^6        16^5       16^4   16^3      16^2 16^1   16^0
+268,435,456 16,777,216 1,048,576  65,536   4,096    256    16      1
+
+
+DEC (10 DIGITS -> 0 to 9)   HEX (16 DIGITS -> 0 to f)
+0     0
+1     1
+2     2
+3     3
+4     4
+5     5
+6     6
+7     7  
+8     8
+9     9
+10    a
+11    b
+12    c
+13    d
+14    e
+15    f
+16    10
